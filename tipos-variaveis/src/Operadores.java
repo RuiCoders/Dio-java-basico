34d4ded72819b48b8d95b9bd8c205732c078dcc4
Rui Andrade <ruyx@live.com>
2023-08-25 20:24:47 -0300

    incremento e decremento

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,26 +1,46 @@
 public class Operadores {
     public static void main(String[] args) {
-        /*  concatenando textos
-        String nomeCompleto = "Linguagem " + "Java";
-        System.out.println(nomeCompleto);
+        // concatenando textos
 
-        String concatenacao = "?";
+        /*
+         * String nomeCompleto = "Linguagem " + "Java";
+         * System.out.println(nomeCompleto);
+         * 
+         * String concatenacao = "?";
+         * 
+         * concatenacao = 1+1+1+"1";
+         * System.out.println(concatenacao);
+         * 
+         * concatenacao = 1+"1"+1+1;
+         * System.out.println(concatenacao);
+         * 
+         * concatenacao = 1+"1"+1+"1";
+         * System.out.println(concatenacao);
+         * 
+         * concatenacao = "1"+1+1+1;
+         * System.out.println(concatenacao);
+         * 
+         * concatenacao = "1"+(1+1+1);
+         * System.out.println(concatenacao);
+         */
 
-        concatenacao = 1+1+1+"1";
-        System.out.println(concatenacao);
+        // Operadores de incremento e decremento
 
-        concatenacao = 1+"1"+1+1;
-        System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+"1";
-        System.out.println(concatenacao);
-
-         concatenacao = "1"+1+1+1;
-        System.out.println(concatenacao);
-
-         concatenacao = "1"+(1+1+1);
-        System.out.println(concatenacao);*/
-
-    
+       /* int numero = 5;
+        int numero2 = 3;
+        numero = -numero;
+        // o numero estava positivo com atribuição o numero ficou negativo
+        System.out.println(numero);
+        // realizzei a impressao e mesmo -5
+        numero = -numero;
+        // realizei a troca do operador aritimentico por - + - retorna positivo 5
+        System.out.println(numero);
+        numero = ++numero;
+        numero2= --numero2;
+        // incremento de antes de impressão somando somente mais 1
+        // incremento depois numero ++ somando somente mais 1
+        System.out.println(numero);
+        System.out.println(numero2);
+        */
     }
 }
