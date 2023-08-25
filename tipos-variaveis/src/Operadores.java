539ebba9bc958dd241fc8901e000c08f10d8bc65
Rui Andrade <ruyx@live.com>
2023-08-25 20:39:04 -0300

    booleano true e false

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -26,21 +26,36 @@ public class Operadores {
 
         // Operadores de incremento e decremento
 
-       /* int numero = 5;
-        int numero2 = 3;
-        numero = -numero;
-        // o numero estava positivo com atribuição o numero ficou negativo
-        System.out.println(numero);
-        // realizzei a impressao e mesmo -5
-        numero = -numero;
-        // realizei a troca do operador aritimentico por - + - retorna positivo 5
-        System.out.println(numero);
-        numero = ++numero;
-        numero2= --numero2;
-        // incremento de antes de impressão somando somente mais 1
-        // incremento depois numero ++ somando somente mais 1
-        System.out.println(numero);
-        System.out.println(numero2);
-        */
+        /*
+         * int numero = 5;
+         * int numero2 = 3;
+         * numero = -numero;
+         * // o numero estava positivo com atribuição o numero ficou negativo
+         * System.out.println(numero);
+         * // realizzei a impressao e mesmo -5
+         * numero = -numero;
+         * // realizei a troca do operador aritimentico por - + - retorna positivo 5
+         * System.out.println(numero);
+         * numero = ++numero;
+         * numero2= --numero2;
+         * // incremento de antes de impressão somando somente mais 1
+         * // incremento depois numero ++ somando somente mais 1
+         * System.out.println(numero);
+         * System.out.println(numero2);
+         */
+
+        // Operadores Boolenos true /false
+
+        /*
+         * boolean verdadeira = true;
+         * boolean falsa = false;
+         * // transformando a mesma com o sinal de negação
+         * verdadeira = !verdadeira;
+         * falsa = !falsa;
+         * System.out.println(verdadeira);
+         * System.out.println(falsa);
+         */
+
+        // 
     }
 }
