d8bbbee6f33d90d39051dd5572c3c7b7b875a5c8
Rui Andrade <ruyx@live.com>
2023-08-29 21:20:33 -0300

    desafio da DIO concluido / Conta bancaria

---
+++ conta-banco-desafio/src/ContaTerminal.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+        String agencia, nomeCliente;
+        double saldo;
+        System.out.println("-------------------- CONTA BANCÁRIA -------------------- \n");
+        System.out.println("Por favor, digite o número da Conta: ");
+        numero = scanner.nextInt();
+
+        System.out.println("Digite sua Agencia: ");
+        agencia = scanner.next();
+        
+        scanner.nextLine();
+          /*
+         * Neste código, após a leitura da agência com scanner.next(),
+         * nós usamos scanner.nextLine() para consumir a quebra de linha pendente no
+         * buffer.
+         * Isso permite que você leia o nome do cliente corretamente, evitando que o
+         * programa pule essa entrada.
+         */
+        System.out.println("Digite seu nome completo:");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Entre com valor inicial depositado na conta:");
+        saldo = scanner.nextDouble();
+
+        System.out.println("-------------------- STATUS CONTA BANCÁRIA -------------------- \n");
+        System.out.printf("Olá " + nomeCliente + ", \n" + 
+                "Obrigado por criar uma conta em nosso banco, \n" + 
+                        "sua agência é "
+                + agencia + ", \n" + 
+                        "conta " + numero + ", \n" + 
+                                "e seu saldo R$ " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+
+    }
+}
