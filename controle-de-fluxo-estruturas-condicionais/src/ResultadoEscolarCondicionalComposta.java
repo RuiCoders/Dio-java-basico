6c1e48bb887b10cdf4625f92540c5761b4510d06
Rui Andrade <ruyx@live.com>
2023-08-30 19:46:29 -0300

    estruturas condicionais

---
+++ controle-de-fluxo-estruturas-condicionais/src/ResultadoEscolarCondicionalComposta.java
@@ -0,0 +1,9 @@
+public class ResultadoEscolarCondicionalComposta {
+    public static void main(String[] args) {
+        int nota = 10;
+        if (nota >= 7)
+            System.out.println("Aprovado");
+        else
+            System.out.println("Reprovado");
+    }
+}
---
+++ controle-de-fluxo-estruturas-condicionais/src/SwitchCase.java
@@ -0,0 +1,19 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        String plano = "T";
+
+        switch (plano) {
+            case "T": {
+                System.out.println("5GB Youtube");
+            
+            }
+            case "M": {
+                System.out.println("Whats e Instagem gratis");
+                break;
+            }
+            case "B": {
+                System.out.println("100 inutos de ligação");
+            }
+        }
+    }
+}
