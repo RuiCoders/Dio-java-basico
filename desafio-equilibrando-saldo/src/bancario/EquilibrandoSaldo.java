8370311bbed630c97fd8f1e5ebc47ad6f14d87ba
Rui Andrade <ruicoders@gmail.com>
2023-09-27 16:45:14 -0300

    Saldo, deposito e retirada

---
+++ desafio-equilibrando-saldo/src/bancario/EquilibrandoSaldo.java
@@ -0,0 +1,26 @@
+package bancario;
+
+import java.util.Scanner;
+
+public class EquilibrandoSaldo {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		double saldoAtual = scanner.nextDouble();
+
+		System.out.print("Digite o valor do depósito: ");
+		double valorDeposito = scanner.nextDouble();
+
+		System.out.print("Digite o valor da retirada: ");
+		double valorRetirada = scanner.nextDouble();
+		scanner.close();
+		
+
+		saldoAtual = saldoAtual + valorDeposito;
+		saldoAtual = saldoAtual - valorRetirada;
+		
+		System.out.printf("Saldo atualizado na conta:  %.1f", saldoAtual);
+	}
+
+}
